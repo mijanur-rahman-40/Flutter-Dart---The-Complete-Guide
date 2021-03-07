8522987ef3ed1d64adc61acf6841652e47a5ad63
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2021-03-07 13:13:45 +0600

    updated

--- Maximilian Schwarzmuller/12. firebase_image_upload_push_notifications _building _a_chat_app/lib/main.dart
+++ Maximilian Schwarzmuller/12. firebase_image_upload_push_notifications _building _a_chat_app/lib/main.dart
@@ -1,40 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/screens/chat_screen.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-        title: 'Flutter Chat',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.amberAccent,
-          errorColor: Colors.red,
-          secondaryHeaderColor: Colors.white,
-          fontFamily: 'Raleway',
-          iconTheme: ThemeData.light()
-              .iconTheme
-              .copyWith(color: Colors.purple, size: 25),
-          textTheme: ThemeData.light().textTheme.copyWith(
-                headline1: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                    color: Colors.white),
-                headline2: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-                headline3: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-                bodyText1: TextStyle(fontSize: 20),
-              ),
-        ),
-        home: ChatScreen(),
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amberAccent,
+        errorColor: Colors.red,
+        secondaryHeaderColor: Colors.white,
+        fontFamily: 'Raleway',
+        iconTheme: ThemeData.light()
+            .iconTheme
+            .copyWith(color: Colors.purple, size: 25),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.white),
+              headline2: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.purple),
+              headline3: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              bodyText1: TextStyle(fontSize: 20),
+            ),
+      ),
+      home: ChatScreen(),
     );
   }
 }
