ed01ec1091d1421cf396ec859110d94be32a3ee4
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2021-03-11 00:27:22 +0600

    updated

--- Maximilian Schwarzmuller/13. running_native_swift_objectiveC_java_or_kotlin_code/android/app/src/main/java/com/example/flutter_complete_guide/MainActivity.java
+++ Maximilian Schwarzmuller/13. running_native_swift_objectiveC_java_or_kotlin_code/android/app/src/main/java/com/example/flutter_complete_guide/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    // GeneratedPluginRegistrant.registerWith(this);
 
     new MethodChannel(getFlutterView(), "course.flutter.dev/battery").setMethodCallHandler(
       new MethodCallHandler() {
