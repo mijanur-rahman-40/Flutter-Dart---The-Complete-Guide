be0d0496c91b43c996a9b3d32dc1dab631096e60
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2021-03-16 09:08:54 +0600

    updated

---
+++ Maximilian Schwarzmuller/11. using_native_device_features_camera_maps_location/lib/config/secrets.dart
@@ -0,0 +1,4 @@
+class CONFIG {
+  static const String GOOGLE_API_KEY =
+      'AIzaSyB744oAeY0-F_1OSlGHlwm9pRs6b4WFxJ0';
+}
--- Maximilian Schwarzmuller/11. using_native_device_features_camera_maps_location/lib/helpers/location_helpers.dart
+++ Maximilian Schwarzmuller/11. using_native_device_features_camera_maps_location/lib/helpers/location_helpers.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:myapp/config/secrets.dart';
 
-const GOOGLE_API_KEY = 'AIzaSyB744oAeY0-F_1OSlGHlwm9pRs6b4WFxJ0';
 
 class LocationHelper {
   // return a string image url
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$CONFIG.GOOGLE_API_KEY';
+    
   }
 
   // return a address
@@ -16,7 +17,7 @@ class LocationHelper {
   static Future<String> getPlaceAddress(
       double latitude, double longitude) async {
     final url =
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$GOOGLE_API_KEY';
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$CONFIG.GOOGLE_API_KEY';
     final response = await http.get(url);
     // 0 index means most relevent address
     return json.decode(response.body)['results'][0]['formatted_address'];
